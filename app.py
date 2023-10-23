c4d159b0196a1070c13775fd4188d0b0910e0f82
oliveiracluiza <146853756+oliveiracluiza@users.noreply.github.com>
2023-10-23 12:13:23 +0000

    atualizar

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.set_page_config('Luiza - ECOP06', 'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
+st.set_page_config(
+    'Luiza - ECOP06', 
+    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png'
+    )
+
 st.title('Página Demo ECOP06')
-esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+
+esportes = pd.read_csv(
+    'https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv', 
+    encoding='latin-1'
+    )
+
 st.dataframe(esportes)
